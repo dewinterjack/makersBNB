a7bc0dad3c76f6c32eb30e359657af30bc838ddf
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-23 17:36:43 +0100

    Database can be created with a users collection.

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -4,10 +4,13 @@ var url = "mongodb://localhost:27017/makersbnb";
 
 gulp.task('createdb', function() {
   // take care of a db that already exists.
-  console.log('Creating database...');
+  console.log('Setting up database...');
   MongoClient.connect(url, function(err, db){
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
+    var dbo = db.db("makersbnb");
+    dbo.createCollection("users", function(err, res) {
+      if (err) throw err;
+      console.log("Database created!");
+      db.close();
+    });
   });
 });
