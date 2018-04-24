6272ba3366bfd28a26bc81307a9941770c6e8ff1
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-24 14:02:59 +0100

    refactor, removed bin jar.

--- app.js
+++ app.js
@@ -8,14 +8,11 @@ var index = require('./routes/index');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/spaces', spaces);
-
-
-
 
 app.listen(9000, () => console.log('Example app listening on port 9000!'));
