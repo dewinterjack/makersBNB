2f3fe07f25442a0694cd9718f09d80c56604785a
Matthew <matt_burgoyne@live.co.uk>
2018-04-24 16:03:31 +0100

    two feature tests written for user to see spaces and add a space and see it displayed on the view spaces

--- tests/features/homeSpec.js
+++ tests/features/homeSpec.js
@@ -14,8 +14,31 @@ module.exports = {
     }, 100);
   },
 
-  'test home page' : function (browser) {
+  'test viewing spaces' : function (browser) {
     browser
-      .url('http://localhost:9000/')
+      .url('http://localhost:9000/spaces')
+      .assert.containsText('makers')
+      .assert.containsText('50-53 commerical street')
+      .assert.containsText('1,000,000');
+   },
+
+   'test viewing an added space' : function (browser) {
+    browser
+      .url('http://localhost:9000/spaces')
+      .click('button[name=addspace')
+      .pause(1000)
+      .setValue('input[name=spacename]', 'buckingham palace')
+      .setValue('input[name=spaceaddress]', 'westminster, london, sw1a 1aa')
+      .setValue('input[name=spaceprice]', '2,000,000')
+      .click('button[name=submitspace')
+      .pause(1000)
+      .assert.containsText('buckingham palace')
+      .assert.containsText('westminster, london, sw1 1aa')
+      .assert.containsText('2,000,000');
+   },
+
+   'test adding a description to a space' : function(browser) {
+     browser
+     .url()
    }
 };
