56ae79f04da63ebdc5a5b7ec269f801b9f8f1ad6
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-24 18:49:11 +0100

    refactor

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -4,7 +4,8 @@ var url = "mongodb://localhost:27017/makersbnb_test";
 
 gulp.task('createTestDB', function() {
 	console.log('Setting up database...');
-	mongoose.connect(url);
+	var c = mongoose.connect('mongodb://localhost:27017/makersbnb_test');
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
+	module.exports = c;
 });
