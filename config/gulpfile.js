60df9a28f91bdabaad712be99032fc3d06e8ab27
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-24 11:06:48 +0100

    ORM set up with mongoose

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -1,16 +1,15 @@
 var gulp = require('gulp');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/makersbnb";
+var mongoose = require('mongoose');
+var url = "mongodb://localhost:27017/makersbnb_test";
 
-gulp.task('createdb', function() {
-  // take care of a db that already exists.
-  console.log('Setting up database...');
-  MongoClient.connect(url, function(err, db){
-    var dbo = db.db("makersbnb");
-    dbo.createCollection("users", function(err, res) {
-      if (err) throw err;
-      console.log("Database created!");
-      db.close();
-    });
-  });
+// order of execution
+// 1. run app (node app.js)
+// 2. createdb
+
+gulp.task('createTestDB', function() {
+	// take care of a db that already exists.
+	console.log('Setting up database...');
+	mongoose.connect(url);
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
 });
