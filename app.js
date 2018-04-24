c6c5244317471c8eadeef7fcd1bdb2607599ad50
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-24 14:50:08 +0100

    spaces route created.

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-var express = require('express');
-var path = require('path');
+let express = require('express');
+let path = require('path');
 
-var users = require('./routes/users');
-var spaces = require('./routes/spaces');
-var index = require('./routes/index');
-
-var app = express();
+let spaces = require('./routes/spaces');
+let app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', index);
-app.use('/users', users);
 app.use('/spaces', spaces);
 
 app.listen(9000, () => console.log('Example app listening on port 9000!'));
