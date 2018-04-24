4392c9a40dda1d0fa6be21527f7b056ac7b4dc17
Daniel Miller <danielmiller5791@gmail.com>
2018-04-24 16:52:17 +0100

    temporary stagin

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let path = require('path');
 
-let users = require('./routes/users');
-let spaces = require('./routes/spaces');
-let index = require('./routes/index');
+//let users = require('./routes/users');
+//let spaces = require('./routes/spaces');
+//let index = require('./routes/index');
 
 let app = express();
 
@@ -11,8 +11,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', index);
-app.use('/users', users);
-app.use('/spaces', spaces);
+//app.use('/', index);
+//app.use('/users', users);
+//app.use('/spaces', spaces);
 
 app.listen(9000, () => console.log('Example app listening on port 9000!'));
