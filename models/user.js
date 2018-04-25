687c88681ec71e7f56fc9c0defd3b9debcba8016
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-25 16:44:52 +0100

    work on passport config

--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
-var Schema = mongoose.Schema,
-	ObjectId = Schema.ObjectId;
+var Schema = mongoose.Schema;
 
 var UserSchema = new Schema(
 	{
