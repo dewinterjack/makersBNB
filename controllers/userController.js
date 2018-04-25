93ea4eeb7317c5a673ca8efaf285c076d56d2ada
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-25 13:59:29 +0100

    added some work for post pug data

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,17 @@ exports.createUserGet = function(req, res) {
 };
 
 exports.createUserPost = function(req, res){
-
+	var email = req.body.email;
+	bcrypt.hash(req.body.password, null, null, function(err, hash){
+		var user = new User({email:email, password:hash});
+		user.save().then(function(newUser){ // need to make mongo ready
+			console.log("Added " + email + " to the database!");
+			// req.session.regenerate(function(){
+			// 	res.redirect('/index');
+			// 	req.session.user = user;
+			// });
+		})
+	});
 };
 
 exports.showUserProfile = function(req, res){
